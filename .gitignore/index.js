1361ae3bfa8901297a3ebaa7b9b5dfb76acdccb0
AlexFPS152 <54991248+AlexFPS152@users.noreply.github.com>
2019-12-02 17:54:10 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("NjE1ODIyNDQ4MTc2MjY3MjY1.XbhNSQ.gPBXap-4bNZjoYql8GOLMEjCcoo");
+
+client.on('guildMemberAdd', member =>{
+    member.guild.channels.get('632844709248892947').send('**Bienvenue sur le serveur de Gothan City !** ' + member.user + ' **Nous sommes désormais ! ** ' + member.guild.memberCount);
+    member.addRole('610148648973631675')
+    console.log(+1)
+})
+
+
+client.on('guildMemberRemove', member =>{
+    member.guild.channels.get('632935684872863754').send('**Merci à toi, et bonne chance !** ' + member.user);
+    console.log(-1)
+
+})
