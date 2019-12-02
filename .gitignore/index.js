019121475b1bdf44a2804a318e223bb3e4f1323c
AlexFPS152 <54991248+AlexFPS152@users.noreply.github.com>
2019-12-02 18:04:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login("NjE1ODIyNDQ4MTc2MjY3MjY1.XeVDsQ.a4VYf9Rr_f4hem-1KtzTfU1y6To");
+client.login(process.env.TOKEN);
 
 client.on('guildMemberAdd', member =>{
     member.guild.channels.get('632844709248892947').send('**Bienvenue sur le serveur de Gothan City !** ' + member.user + ' **Nous sommes désormais ! ** ' + member.guild.memberCount);
